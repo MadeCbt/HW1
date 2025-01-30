33ef70eeecc6fa81c43204f797af33ddf550967f
Jesse <150229859+MadeCbt@users.noreply.github.com>
2025-01-30 00:03:17 -0600

    Update HW1.java

--- HW1.java
+++ HW1.java
@@ -1,5 +1,5 @@
 /*
- * *** Jesse Amodemaja / SECTION  HERE ***
+ * *** Jesse Amodemaja / 001 ***
  *
  * Homework # 1 (Programming Assignment). This Java class defines some basic
  * manipulation operations on Linked-Lists and Stacks.
